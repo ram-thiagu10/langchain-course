a087c5626bd60410d2dd1e9b8aba807a1a2d678f
Ram Thiagu <ram.thiagu10@gmail.com>
2026-07-13 14:19:14 +0530

    tavily search and custom search commented

--- main.py
+++ main.py
@@ -7,33 +7,35 @@ from langchain.tools import tool
 from langchain_core.messages import HumanMessage
 from langchain_groq import ChatGroq
 from langchain_tavily import TavilySearch
+# from tavily import TavilyClient
 
-@tool
-def search(query: str) -> str:
-    """
-    Search for a query and return the results.
-    Args:
-        query (str): The search query.
-    Returns:
-        str: The search results.
-    """
-    print(f"Searching for: {query}")
-    return f"Results for '{query}'"
+# tavily = TavilyClient()
+
+# @tool
+# def search(query: str) -> str:
+#     """
+#     Search for a query and return the results.
+#     Args:
+#         query (str): The search query.
+#     Returns:
+#         str: The search results.
+#     """
+#     print(f"Searching for: {query}")
+#     return tavily.search(query=query)
 llm = ChatGroq(
     model_name="qwen/qwen3-32b",
     temperature=0,
     api_key=os.getenv("GROQ_API_KEY")
     )
-tavily = TavilySearch(max_results=5)
 # tools = [search]
-tools = [tavily]
+tools = [TavilySearch()]
 agent = create_agent(model=llm, tools=tools)
 def main():
     print("Hello from langchain-course!")
 
     response = agent.invoke({
         "messages": [
-            HumanMessage(content="what udhayanidhi stalin doing today?"),
+            HumanMessage(content="search for 3 job opening for ai engineer in hyderabad"),
         ]
     })
     print(f"Agent result: {response['messages'][-1].content}")
