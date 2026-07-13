ac3965de26d1bce016ac96c60b82f8b03195f1e4
Ram Thiagu <ram.thiagu10@gmail.com>
2026-07-13 15:30:15 +0530

    Structured output generation with pydantic

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 from dotenv import load_dotenv
 import os
 load_dotenv()
-
+from typing import List
+from pydantic import BaseModel, Field
 from langchain.agents import create_agent
 from langchain.tools import tool
 from langchain_core.messages import HumanMessage
 from langchain_groq import ChatGroq
 from langchain_tavily import TavilySearch
+
+class Source(BaseModel):
+    """Schema for a source used by the agent"""
+    url:str = Field(description="The URL of the source")
+
+class AgentResponse(BaseModel):
+    """Schema for the agent response"""
+    answer: str = Field(description="The answer from the agent")
+    sources: List[Source] = Field(default_factory = list, description="The sources used by the agent")
+
 # from tavily import TavilyClient
 
 # tavily = TavilyClient()
@@ -23,22 +34,22 @@ from langchain_tavily import TavilySearch
 #     print(f"Searching for: {query}")
 #     return tavily.search(query=query)
 llm = ChatGroq(
-    model_name="qwen/qwen3-32b",
+    model_name="llama-3.3-70b-versatile",
     temperature=0,
     api_key=os.getenv("GROQ_API_KEY")
     )
 # tools = [search]
 tools = [TavilySearch()]
-agent = create_agent(model=llm, tools=tools)
+agent = create_agent(model=llm, tools=tools, response_format=AgentResponse)
 def main():
     print("Hello from langchain-course!")
 
     response = agent.invoke({
         "messages": [
-            HumanMessage(content="search for 3 job opening for ai engineer in hyderabad"),
+            HumanMessage(content="search for 3 job opening for 5+ years ai engineer in hyderabad"),
         ]
     })
-    print(f"Agent result: {response['messages'][-1].content}")
+    print("Agent response:", response)
     
 
 if __name__ == "__main__":
